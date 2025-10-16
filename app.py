b5bb90b6c2613676b2f78042d9fc38a51a7d551e
helloPhysics <hellophysics631@gmail.com>
2025-10-16 20:41:11 +0100

    Banner update

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ st.set_page_config(page_title="GraPhycs3", layout="wide")
 
 # The original st.image was removed because the asset was unavailable in this environment.
 # If running locally and 'assets/GraPhycs.png' exists, uncomment the line below:
-st.image("assets/GraPhycs.png", width=1000)
+st.image("GraPhycs.png", width=1000)
 
 # Using a text placeholder instead:
 st.markdown("GraPhycs 📊")
